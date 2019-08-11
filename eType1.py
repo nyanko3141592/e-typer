b3bb33a1edb3e2427cfce04d680349e951c9c121
NanokiTakahashi <takahashinaoki521@gmail.com>
2019-08-11 23:45:16 +0900

    ログインまでできた

--- eType1.py
+++ eType1.py
@@ -2,50 +2,35 @@ import time
 from selenium import webdriver
 import logging
 import sys
+import secretThings
 
 
 def main():
+    #logging.debugの設定　省略可能
     try:debugLevel = int(sys.argv[1])
     except:debugLevel = 0
 
     if debugLevel == 1:
         logging.basicConfig(level=logging.DEBUG)
-    elif debugLevel == 2:
-        logging.basicConfig(level= logging.WARNING)
     else:
         logging.basicConfig(level=logging.CRITICAL)
 
+    #chromeを開く
     driver = webdriver.Chrome('./chromedriver')
     driver.get("https://www.e-typing.ne.jp/member/")
     logging.debug("show browser of eTyping")
-    driver.find_element_by_id("#mail").send_keys("strings")
-    try:
-        # driver.find_element_by_xpath('//div[@id="start_btn"]').click()
-        # start
-        start_button1 = driver.find_element_by_xpath("//a[@title='腕試しレベルチェック']")
-        start_button1.click()
-        logging.debug("click is succeeded")
-    except:
-        driver.quit()
-        logging.debug("click is failed")
-    time.sleep(10)
 
-    # try:
-    #     driver.find_element_by_xpath("").click()
-    #     logging.debug("click is succeeded")
-    # except:
-    #     logging.debug("=====click is failed=====")
-    #     driver.quit()
+    #login
+    driver.find_element_by_id("mail").send_keys(secretThings.mail)
+    driver.find_element_by_id("password").send_keys(secretThings.password)
+    driver.find_element_by_xpath("//*[@id=\"login_btn\"]").click()
 
-    # text = driver.find_element_by_css_selector('#register-email')
-    # print(text.get_attribute("placeholder"))
-    # textのときは.text
-    # それ以外は.get_attribute
+
     driver.quit()
     logging.debug('====DONE====')
 
 if __name__ == "__main__":
     try:main()
-    except(KeyboardInterrupt):pass
+    except(KeyboardInterrupt):logging.debug("keyinterrupt")
     except:pass
 
